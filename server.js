3ebf72ff06961de44bb4beef0022909f5d098e4c
DenimCity <altidorj1@gmail.com>
2019-11-07 08:57:07 -0500

    imported seeder

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const db = require('./server/models/db');
+const seed = require('./server/models/seed/seed-db');
 // setup the Express middlware
 require('./server/middleware/middleware')(app);
 
 // setup the api
 require('./server/api')(app);
 
-app.listen(port, () => {
-  console.log('running server on port ' + port);
-})
+// Connecting to Database
+db.sequelize
+	.sync({ force: true })
+	.then(() => {
+		seed.insert();
+	})
+	.then(() => {
+		app.listen(port, () => {
+			console.log('running server on port ' + port);
+		});
+	});
